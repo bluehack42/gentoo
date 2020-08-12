de745be157bcd20b46b47de579d201553b19364e
blue <blue@sourcecode.li>
2020-08-12 21:04:01 +0200

    Wait for network in gitclone

--- usb/gitclone.sh
+++ usb/gitclone.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-sleep 20
+while ! ping -c 1 -W 1 1.1.1.1; do
+  echo "Wait for network"
+  sleep 1
+done
+
 git clone https://github.com/bluehack42/gentoo.git 
 cd gentoo/package
 ansible-playbook gentoo-package.yml
