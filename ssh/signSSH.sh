6cad3a30702b374b859ef882badb1d2da450f543
blue <blue@sourcecode.li>
2021-09-05 20:38:50 +0200

    Update sign ssh keys

--- ssh/signSSH.sh
+++ ssh/signSSH.sh
@@ -2,5 +2,5 @@
 
 set -e
 
-ssh-keygen -s ca $(gopass vps/ca > ca; chmod 600 ca) -I $USER -n $USER -V +1d ~/.ssh/id_rsa.pub
+ssh-keygen -s ca $(gopass show vps/ca > ca; chmod 600 ca) -I $USER -n $USER -V +1d ~/.ssh/id_rsa.pub
 rm ca
